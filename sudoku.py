3f5ebf1ee7ace116a4c6a73aac2c9a58d198209c
z252zhu <zhuzihang1207@outlook.com>
2022-12-05 03:54:24 -0500

    Add menu control to Sudoku class

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,16 @@
 import pygame
 from gameboard import GameBoard
-from boarddata import BoardData
-from copy import deepcopy
 
 BOARD_SIZE = 630
 
 class Sudoku:
-    def __init__(self, board):
+    def __init__(self):
         # set up basic game parameters
+        self.game = GameBoard()
+        self.difficulty = 0
+        self.board_id = 0
+        self.draw_status = 0
+        self.max_board_id = len(self.game.boards[self.difficulty])
         self.x = 0
         self.y = 0
         self.cell = int(BOARD_SIZE / 9)
@@ -17,7 +20,7 @@ class Sudoku:
         # initiate pygame
         pygame.font.init()
         self.number_font = pygame.font.SysFont("Helvetica", 35)
-        self.display_font = pygame.font.SysFont("Helvetica", 20, bold = True)
+        self.msg_font = pygame.font.SysFont("Helvetica", 20, bold = True)
         self.title_font = pygame.font.SysFont("Helvetica", 50, bold = True)
         self.main_window = pygame.display.set_mode((BOARD_SIZE + 2, BOARD_SIZE + 50))
         pygame.display.set_caption("SUDOKU GAME")
@@ -25,7 +28,6 @@ class Sudoku:
         pygame.display.set_icon(sudoku_icon)
         
         # set up cell and box selection parameters
-        self.game = GameBoard(board)
         self.selectbox = pygame.Rect((0, 0, self.cell + 1, self.cell + 1))
         self.h_background = pygame.Rect((0, 0, self.cell + 1, BOARD_SIZE))
         self.v_background = pygame.Rect((0, 0, BOARD_SIZE, self.cell + 1))
@@ -33,67 +35,68 @@ class Sudoku:
         self.box_background = pygame.Rect((0, 0, self.box, self.box))
     
     def draw_title(self):
+        self.main_window.fill((255,255,255))
         # game title
         game_title = self.title_font.render("Sudoku Game", 1, (0, 0, 0))
-        self.main_window.blit(game_title, (120, 200))
+        self.main_window.blit(game_title, (110, 250))
         # selection hint
-        hint = self.display_font.render("Press ENTER to select: ", 1, (0, 0, 0))
-        self.main_window.blit(hint, (10, 470))
+        hint = self.msg_font.render("Press ENTER to select: ", 1, (0, 0, 0))
+        self.main_window.blit(hint, (10, 600))
     
-    def select_difficulty(self, diffculty_i):
-        # draw difficulty selection
-        difficulty = ["EASY", "MEDIUM", "HARD", "EXPERT", "EVIL"]
-        if diffculty_i == 0: 
-            msg = " " * 4 + difficulty[diffculty_i] + " ->"
-        elif diffculty_i in range(1, 4):
-            msg = "<- " + difficulty[diffculty_i] + " ->"
+    def select_difficulty(self, selection):
+        self.difficulty += selection
+        if self.difficulty < 0:
+            self.difficulty = 0
+        elif self.difficulty > 4:
+            self.difficulty = 4
+    
+    def draw_difficulty(self):
+        str_difficulty = ["EASY", "MEDIUM", "HARD", "EXPERT", "EVIL"]
+        if self.difficulty == 0:
+            msg = " " * 4 + str_difficulty[self.difficulty] + " ->"
+        elif self.difficulty in range(1, 4):
+            msg = "<- " + str_difficulty[self.difficulty] + " ->"
         else:
-            msg = "<- " + difficulty[diffculty_i]
-        text = self.display_font.render(msg, 1, (0, 0, 0))
+            msg = "<- " + str_difficulty[self.difficulty]
+        text = self.msg_font.render(msg, 1, (0, 0, 0))
         self.main_window.blit(text, self.msg_pos)
 
-    def select_map_id(self, board_id):
-        # board selection
-        numberStr = str(boardNumber + 1)
-        if boardNumber == 0: 
-            msg = " " * 4 + numberStr + " ->"
-        elif 1 <= boardNumber and boardNumber <= boardNumber_max - 1: 
-            msg = "<- " + numberStr + " ->"
-        else: 
-            msg = "<- " + numberStr + " " * 4
-        text = displayFont.render(msg, 1, (0, 0, 0))
-        sudokuInterface.blit(text, msg_pos)
+    def select_board(self, selection):
+        self.board_id += selection
+        self.max_board_id = len(self.game.boards[self.difficulty])
+        if self.board_id < 0:
+            self.board_id = 0
+        elif self.board_id > self.max_board_id - 1:
+            self.board_id = self.max_board_id
     
-    def mouse_pos(self, pos):
-        if pos[0] < BOARD_SIZE and pos[1] < BOARD_SIZE:
-            self.x = int(pos[0] // self.cell)
-            self.y = int(pos[1] // self.cell)
-            print(self.x, self.y)
-            # update selection box
-            self.selectbox.x = self.x * self.cell
-            self.selectbox.y = self.y * self.cell
-            # update horizontal background
-            self.h_background.x = self.x * self.cell
-            self.h_background.y = 0
-            # update vertical background
-            self.v_background.x = 0
-            self.v_background.y = self.y * self.cell
-            # update box background
-            self.box_background.x = (self.x // 3) * self.box
-            self.box_background.y = (self.y // 3) * self.box
-            # update cell background
-            self.cell_background.x = self.x * self.cell
-            self.cell_background.y = self.y * self.cell
+    def draw_board_id(self):
+        str_board_id = str(self.board_id + 1)
+        if self.board_id == 0: 
+            msg = " " * 4 + str_board_id + " ->"
+        elif self.board_id in range(1, self.max_board_id - 1):
+            msg = "<- " + str_board_id + " ->"
+        else:
+            msg = "<- " + str_board_id
+        text = self.msg_font.render(msg, 1, (0, 0, 0))
+        self.main_window.blit(text, self.msg_pos)
     
-    def move_selectbox(self, dx, dy):
-        if dx < 0: 
-            self.x = max(0, self.x - 1)
-        elif dx > 0: 
-            self.x = min(8, self.x + 1)
-        if dy < 0: 
-            self.y = max(0, self.y - 1)
-        elif dy > 0: 
-            self.y = min(8, self.y + 1)
+    def update_selectionbox(self, x, y, mouse_input):
+        # mouse input
+        if mouse_input:
+            if x < BOARD_SIZE and x < BOARD_SIZE:
+                self.x = int(x // self.cell)
+                self.y = int(y // self.cell)
+        # keyboard input
+        else:
+            if x < 0:
+                self.x = max(0, self.x - 1)
+            elif x > 0:
+                self.x = min(8, self.x + 1)
+            if y < 0:
+                self.y = max(0, self.y - 1)
+            elif y > 0:
+                self.y = min(8, self.y + 1)
+        
         print(self.x, self.y)
         # update selection box
         self.selectbox.x = self.x * self.cell
@@ -145,54 +148,79 @@ class Sudoku:
                     self.main_window.blit(text, (j * self.cell + 26, i * self.cell + 20))
 
 def main():
-    data = BoardData()
-    sudoku = Sudoku(data.boards[0][0])
+    sudoku = Sudoku()
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                sudoku.mouse_pos(pos)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    sudoku.move_selectbox(-1, 0)
-                if event.key == pygame.K_RIGHT:
-                    sudoku.move_selectbox(1, 0)
-                if event.key == pygame.K_UP:
-                    sudoku.move_selectbox(0, -1)
-                if event.key == pygame.K_DOWN:
-                    sudoku.move_selectbox(0, 1)
-                if event.key == pygame.K_1:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 1)
-                if event.key == pygame.K_2:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 2)
-                if event.key == pygame.K_3:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 3)
-                if event.key == pygame.K_4:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 4)
-                if event.key == pygame.K_5:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 5)
-                if event.key == pygame.K_6:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 6)
-                if event.key == pygame.K_7:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 7)
-                if event.key == pygame.K_8:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 8)
-                if event.key == pygame.K_9:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 9)
-                if event.key == pygame.K_RETURN:
-                    sudoku.game.solvegame()
-                if event.key == pygame.K_BACKSPACE:
-                    sudoku.game.update_cell(sudoku.x, sudoku.y, 0)
-        sudoku.draw_background()
-        sudoku.draw_board()
-        sudoku.draw_selectbox()
+            if sudoku.draw_status < 2:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        sudoku.board_id = 0
+                        sudoku.draw_status = max(0, sudoku.draw_status - 1)
+                    if event.key == pygame.K_RETURN:
+                        sudoku.draw_status += 1
+                    if event.key == pygame.K_LEFT:
+                        if sudoku.draw_status == 0:
+                            sudoku.select_difficulty(-1)
+                        else:
+                            sudoku.select_board(-1)
+                    if event.key == pygame.K_RIGHT:
+                        if sudoku.draw_status == 0:
+                            sudoku.select_difficulty(1)
+                        else:
+                            sudoku.select_board(1)
+            else:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    sudoku.update_selectionbox(pos[0], pos[1], True)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        sudoku.update_selectionbox(-1, 0, False)
+                    if event.key == pygame.K_RIGHT:
+                        sudoku.update_selectionbox(1, 0, False)
+                    if event.key == pygame.K_UP:
+                        sudoku.update_selectionbox(0, -1, False)
+                    if event.key == pygame.K_DOWN:
+                        sudoku.update_selectionbox(0, 1, False)
+                    if event.key == pygame.K_BACKSPACE:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 0)
+                    if event.key == pygame.K_1:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 1)
+                    if event.key == pygame.K_2:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 2)
+                    if event.key == pygame.K_3:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 3)
+                    if event.key == pygame.K_4:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 4)
+                    if event.key == pygame.K_5:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 5)
+                    if event.key == pygame.K_6:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 6)
+                    if event.key == pygame.K_7:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 7)
+                    if event.key == pygame.K_8:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 8)
+                    if event.key == pygame.K_9:
+                        sudoku.game.update_cell(sudoku.x, sudoku.y, 9)
+                    if event.key == pygame.K_RETURN:# and pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                        sudoku.game.solvegame()
+                    if event.key == pygame.K_ESCAPE:
+                        sudoku.board_id = 0
+                        sudoku.draw_status = max(0, sudoku.draw_status - 1)
+        if sudoku.draw_status == 0:
+            sudoku.draw_title()
+            sudoku.draw_difficulty()
+        elif sudoku.draw_status == 1:
+            sudoku.draw_title()
+            sudoku.draw_board_id()
+        else:
+            sudoku.draw_background()
+            sudoku.draw_board()
+            sudoku.draw_selectbox()
         pygame.display.update()
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
